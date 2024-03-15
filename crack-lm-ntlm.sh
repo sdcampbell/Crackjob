b41202f6cb2e24d14fd1ac864baebda93b83a3d2
Steve Campbell <sdcampbell68@live.com>
2024-03-14 21:29:46 -0400

    Update crack-lm-ntlm.sh

--- crack-lm-ntlm.sh
+++ crack-lm-ntlm.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 if [ -z "$1" ]; then
     echo "No hashfile supplied"
@@ -22,7 +22,7 @@ hashcat -m 1000 --session crackjob -w 4 --potfile-path $(pwd)/ntpotfile ${hashfi
 hashcat -m 1000 --session crackjob -w 4 --potfile-path $(pwd)/ntpotfile -r /usr/share/hashcat/rules/OneRuleToRuleThemAll.rule ${hashfile} /usr/share/hashcat/wordlists/super_wordlist
 #hashcat -m 1000 -w 4 --potfile-path $(pwd)/ntpotfile -r /usr/share/hashcat/rules/OneRuleToRuleThemAll.rule ${hashfile} /usr/share/hashcat/wordlists/super_wordlist --show --user > cracked.txt
 hashcat -m 1000 --session crackjob -a 3 --potfile-path $(pwd)/ntpotfile --increment --increment-min 1 --increment-max 8 --runtime=$seconds ${hashfile} ?a?a?a?a?a?a?a?a
-hashcat -m 1000 --session crackjob -a 3 --potfile-path $(pwd)/ntpotfile --increment --increment-min 1 --increment-max 8 ${hashfile} ?a?a?a?a?a?a?a?a --show --user > cracked.txt
+hashcat -m 1000 --session crackjob -a 3 --potfile-path $(pwd)/ntpotfile --increment --increment-min 1 --increment-max 8 ${hashfile} ?a?a?a?a?a?a?a?a --show > cracked.txt
 
 cracked_hashes=$(cat cracked.txt|wc -l)
 echo ""
@@ -30,7 +30,7 @@ echo "Total hashes: $total_hashes"
 echo "Unique hashes: $unique_hashes"
 echo "Number of accounts with LM enabled: $(cat ${hashfile} | grep -v aad3b435b51404eeaad3b435b51404ee | wc -l)"
 
-cat $1 | grep -v '\$' | cut -d ':' -f 1,4 | sed '/:$/!s/$/:/' > junk1
+cat $1 | grep -v '\$' | cut -d ':' -f 4 | sed '/:$/!s/$/:/' > junk1
 
 cat cracked.txt >> junk1
 
